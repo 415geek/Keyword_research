2ff843f17d399a0cf322d93ac876a874a6592506
c8geek <72575716+415geek@users.noreply.github.com>
2025-10-29 00:59:33 -0700

    Add configuration for data sources and defaults

---
+++ utils/config.py
@@ -0,0 +1,23 @@
+# utils/config.py
+
+DATA_SOURCES = [
+    "https://www.ccyp.com/",
+    "https://www.chineseinla.com/",
+    "http://www.in7788.com/forum.php?mod=forumdisplay&fid=37&filter=hot",
+    "https://www.bay123.com/",
+    "https://tba.huarenca.com/",
+    "https://northerncalifornia.jinbay.com/",
+    "https://www.chineseincalifornia.com/",
+    "https://www.dealmoon.com/",
+    "https://www.1point3acres.com/bbs/",
+    "https://reddit.com/",
+    "https://huaren.us/",
+    "https://www.uscardforum.com/",
+    "https://uscanyin.com/",
+]
+DEFAULT_MAX_PAGES = 2
+DEFAULT_TIMEOUT = 12
+DEFAULT_UA = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0 Safari/537.36"
+)
